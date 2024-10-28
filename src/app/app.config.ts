85fef3580e7f2cb5019b85f7721c7e75c0013165
volinha <viniciusfernandesmct@gmail.com>
2024-10-27 23:16:40 -0300

    feat: componente app controla o fluxo dos dados

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';// Para usar ngModel
+import { FormsModule } from '@angular/forms';
+import { InjectionToken } from '@angular/core';
+import { environment } from '../environment/environment';
+
+export const OPEN_WEATHER_API_KEY = new InjectionToken<string>(
+  'OPEN_WEATHER_API_KEY'
+);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    FormsModule, // Adicione esta linha
+    FormsModule,
+    {
+      provide: OPEN_WEATHER_API_KEY,
+      useValue: environment.openWeatherApiKey,
+    },
   ],
 };
